4d6ffefbf1cf2d41cbda4432a62a39f63be2c1f3
Tiago Borges <tiagoborges899@gmail.com>
2024-09-24 16:05:45 -0300

    feat: adicionado header ao component App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import LoadingSpinner from './components/LoadingSpinner'
 import SearchBar from './components/SearchBar'
 import WeatherCard from './components/WeatherCard'
 import WeatherLocale from './components/WeatherLocale'
+import './styles/App.css'
 
 function App() {
   return (
     <div className='main'>
         <LoadingSpinner />
-        <SearchBar />
+        <header className='weather_header'>
+          <div className='weather_header_logo'>
+            <span className='weather_header_logo_title'>
+              W <span>F</span>
+            </span>
+          </div>
+          <SearchBar />
+        </header>
 
         <div>
           <WeatherLocale />
