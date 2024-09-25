0188ddf5315ab0ec867852e197242d545ae90e8c
Tiago Borges <tiagoborges899@gmail.com>
2024-09-25 13:25:17 -0300

    feat: implementado método para buscar dados do clima

--- src/service/weatherApi.ts
+++ src/service/weatherApi.ts
@@ -1,7 +1,13 @@
 import { BASE_URL } from "./configApi";
 
-export const fetchWeatherData = () => {
-
+export const fetchWeatherData = async(locale: string) => {
+    try {
+        const { data } = await BASE_URL.get(`/current.json?key=${process.env.KEY_API}&q=${locale}&aqi=no`)
+        return data
+    } catch (error: any) {
+        console.error(error.message)
+        return null
+    }
 }
 
 export const fetchForecastData = () => {
