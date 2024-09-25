a901b6584cca320c592ca64a962155637a58b17a
Tiago Borges <tiagoborges899@gmail.com>
2024-09-25 13:42:09 -0300

    feat: chamada do Contexto para pegar localização digitada no input

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,10 +1,14 @@
 import SearchIcon from '@mui/icons-material/Search';
+import { useContext } from 'react';
+import { WeatherContext } from '../../context/WeatherContext';
 
 const SearchBar = () => {
+    const {location, setLocation} = useContext(WeatherContext)
+
     return (
         <div className="search_bar_container">
             <form className="search_bar_form flex items-center b-bottom">
-                <input type="text" className="search_bar_locale font-regular" placeholder='Digite a localidade' />
+                <input type="text" value={location} className="search_bar_locale font-regular" placeholder='Digite a localidade' onChange={(event) => setLocation(event.target.value)}/>
                 <button className="search_bar_search">
                     <SearchIcon className='search_bar_icon_Search' />
                 </button>
