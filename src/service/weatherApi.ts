5dd40e5fe41c7063013a605a7bc44d064bddded1
Tiago Borges <tiagoborges899@gmail.com>
2024-09-26 20:29:44 -0300

    feat: configurado rota com key e locale

--- src/service/weatherApi.ts
+++ src/service/weatherApi.ts
@@ -2,7 +2,7 @@ import { BASE_URL } from "./configApi";
 
 export const fetchWeatherData = async(locale: string) => {
     try {
-        const { data } = await BASE_URL.get(`/current.json?key=${process.env.KEY_API}&q=${locale}&aqi=no`)
+        const { data } = await BASE_URL.get(`/current.json?key=693665a4e54c4be3973160204242409&q=${locale}&aqi=no`)
         return data
     } catch (error: any) {
         console.error(error.message)
