a161fed935723cd7ea4291426f2890fbe191f335
Tiago Borges <tiagoborges899@gmail.com>
2024-09-26 20:38:05 -0300

    feat: criada logica para pegar cidade, hora e data e renderizar no component

--- src/components/WeatherLocale/index.tsx
+++ src/components/WeatherLocale/index.tsx
@@ -1,16 +1,29 @@
-import WbSunnyIcon from '@mui/icons-material/WbSunny';
+// import WbSunnyIcon from '@mui/icons-material/WbSunny';
+import { useEffect, useState } from 'react';
+import { Weather } from '../../models/Weather';
+import useWeather from '../../hooks/useWeather';
 
 const WeatherLocale = () => {
+    const [weatherData, setWeatherData] = useState<Weather>()
+    const data = useWeather()
+
+    useEffect(() => {       
+        if (data.weatherData !== null) {
+            setWeatherData(data.weatherData)
+        }
+    }, [data])
+    
     return (
         <div className="weather_locale_container">
            <section className='weather_locale_temperature_session flex items-center justify-center'>
-                <span className='weather_locale_temperature pr font-regular'>21º</span>
+                <span className='weather_locale_temperature pr font-regular'>{Math.floor(weatherData?.current.temp_c || 0)}º</span>
                 <div className='weather_locale_locale_name column'>
-                    <span className="weather_locale_city_name relative font-regular">São Paulo</span>
-                    <span className="weather_locale_date font-regular">14:09 - Terça, 24 Set 2024</span> 
+                    <span className="weather_locale_city_name relative font-regular">{weatherData?.location.region}</span>
+                    <span className="weather_locale_date font-regular">{weatherData?.location.localtime}</span> 
                 </div>
                 <span className="weather_locale_icon">
-                    <WbSunnyIcon />
+                    <img src={weatherData?.current.condition.icon} alt="ìcone de clime" className='' />
+                    {/* <WbSunnyIcon /> */}
                 </span>
            </section>
         </div>
