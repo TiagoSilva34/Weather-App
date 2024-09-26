d23a4d3cf46a67dc0dbd85c0b5472f4c99ccf17c
Tiago Borges <tiagoborges899@gmail.com>
2024-09-26 20:29:06 -0300

    feat: configurado endpoint com key e locale

--- src/service/configApi.ts
+++ src/service/configApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const BASE_URL = axios.create({
-    baseURL: "http://api.weatherapi.com/v1",
+    baseURL: "https://api.weatherapi.com/v1",
     headers: {
         "Content-Type": "application/json"
     }
