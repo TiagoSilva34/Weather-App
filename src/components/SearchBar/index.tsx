2179c0592109bbd2b3ab15c96e4d2b2d29db40e0
Tiago Borges <tiagoborges899@gmail.com>
2024-09-26 20:33:40 -0300

    feat: criado logica para armazenar local digitado no input de busca de cidade

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,15 +1,21 @@
 import SearchIcon from '@mui/icons-material/Search';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { WeatherContext } from '../../context/WeatherContext';
 
 const SearchBar = () => {
-    const {location, setLocation} = useContext(WeatherContext)
+    const [cityName, setCityName] = useState("")
+    const { setLocation } = useContext(WeatherContext)
+
+    const handleSubmit = (evt: React.FormEvent) => {
+        evt.preventDefault()
+        setLocation(cityName)
+    }
 
     return (
-        <div className="search_bar_container">
+        <div className="search_bar_container" onSubmit={handleSubmit}>
             <form className="search_bar_form flex items-center b-bottom">
-                <input type="text" value={location} className="search_bar_locale font-regular" placeholder='Digite a localidade' onChange={(event) => setLocation(event.target.value)}/>
-                <button className="search_bar_search">
+                <input type="text" value={cityName} className="search_bar_locale font-regular" placeholder='Digite a localidade' onChange={(event) => setCityName(event.target.value)}/>
+                <button type='submit' className="search_bar_search">
                     <SearchIcon className='search_bar_icon_Search' />
                 </button>
             </form>
