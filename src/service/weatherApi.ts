62378db5f89ad34708cbb62db6d22ac87afbe0f4
Tiago Borges <tiagoborges899@gmail.com>
2024-09-28 16:29:10 -0300

    feat: criar variável para usar key de autenticação e criar função para buscar dado da previsão semanal

--- src/service/weatherApi.ts
+++ src/service/weatherApi.ts
@@ -1,8 +1,11 @@
 import { BASE_URL } from "./configApi";
 
+const key = import.meta.env.VITE_API_KEY
+
 export const fetchWeatherData = async(locale: string) => {
     try {
-        const { data } = await BASE_URL.get(`/current.json?key=693665a4e54c4be3973160204242409&q=${locale}&aqi=no`)
+        const { data } = await BASE_URL.get(`/current.json?key=${key}&q=${locale}&aqi=no&lang=pt`)
+        
         return data
     } catch (error: any) {
         console.error(error.message)
@@ -10,6 +13,13 @@ export const fetchWeatherData = async(locale: string) => {
     }
 }
 
-export const fetchForecastData = () => {
-    
+export const fetchForecastData = async(locale: string) => {
+    try {
+        const { data } = await BASE_URL.get(`forecast.json?key=${key}&q=${locale}&days=7&aqi=no&alerts=no&lang=pt`)
+
+        return data
+    } catch (error: any) {
+        console.error(error.message)
+        return null
+    }
 }
