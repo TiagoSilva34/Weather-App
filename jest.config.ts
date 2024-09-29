95707547734f07b3e1c948b039cbb1d39a0f0eaf
Tiago Borges <tiagoborges899@gmail.com>
2024-09-29 14:27:59 -0300

    fix: refatorar o arquivo de configuração do jest

--- jest.config.ts
+++ jest.config.ts
@@ -3,5 +3,11 @@ export default {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
+
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "^.+\\.svg$": "jest-transformer-svg",
+  },
+
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
 };
