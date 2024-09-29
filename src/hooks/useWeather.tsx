435c663e1f0f3a13683f7fe6d02db0c1bae2d000
Tiago Borges <tiagoborges899@gmail.com>
2024-09-29 14:11:59 -0300

    fix: refatorar estrutura do useEffect de chamada da API

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -2,45 +2,16 @@ import { useContext, useEffect, useState } from "react"
 import { WeatherContext } from "../context/WeatherContext"
 import { fetchForecastData, fetchWeatherData } from "../service/weatherApi"
 
+let defaultLocation = "-23.520856837459185,-46.57482654740279"
+
 const useWeather = () => {
     const { location } = useContext(WeatherContext)
     const [weatherData, setWeatherData] = useState(null)
     const [forecastData, setForecastData] = useState(null)
     const [error, setError] = useState(null)
 
-    useEffect(() => {
-        let defaultLocation = ""
-
-        if (!location) {
-            navigator.geolocation.getCurrentPosition(function(location) {
-                defaultLocation = `${location.coords.latitude},${location.coords.longitude}`
-    
-                if (defaultLocation !== "") {
-                    fetchWeatherData(defaultLocation)
-                    .then(response => {
-                        if (response) {
-                            setWeatherData(response)
-                        
-                            fetchForecastData(response.location.name)
-                            .then(forecast => {
-                                if (forecast) setForecastData(forecast)
-                            })
-                            .catch(error => {
-                                setError(error)
-                            })
-                        }
-                    })
-                    .catch(error => {
-                        setError(error)
-                    })
-                    
-                }
-            });
-            
-        }
-        
-        if (location) {
-            fetchWeatherData(location)
+    useEffect(() => {    
+            fetchWeatherData(location || defaultLocation)
                 .then(response => {
                     if (response) {
                         setWeatherData(response)
@@ -56,7 +27,6 @@ const useWeather = () => {
                 .catch(error => {
                     setError(error)
                 })
-        }
     }, [location])
     
     return {weatherData, forecastData, error}    
