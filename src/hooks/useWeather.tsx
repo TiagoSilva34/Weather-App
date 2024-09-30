dd78fce187b082ac0250580f2ae0586c72a4a714
Tiago Borges <tiagoborges899@gmail.com>
2024-09-30 13:27:33 -0300

    feat: adicionar chamada do ForecastPick e criar estado para armazenar dados da previsão

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -1,13 +1,14 @@
 import { useContext, useEffect, useState } from "react"
 import { WeatherContext } from "../context/WeatherContext"
 import { fetchForecastData, fetchWeatherData } from "../service/weatherApi"
+import { ForecastPick } from "../models/Weather"
 
 let defaultLocation = "-23.520856837459185,-46.57482654740279"
 
 const useWeather = () => {
     const { location } = useContext(WeatherContext)
     const [weatherData, setWeatherData] = useState(null)
-    const [forecastData, setForecastData] = useState(null)
+    const [forecastData, setForecastData] = useState<ForecastPick>()
     const [error, setError] = useState(null)
 
     useEffect(() => {    
