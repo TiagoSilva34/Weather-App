01f25d6f8fd1a3e476c1b4bd2c56779c06125941
Tiago Borges <tiagoborges899@gmail.com>
2024-09-30 20:15:37 -0300

    feat: criar função para pedir localização do usuário automaticamente

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -3,32 +3,46 @@ import { WeatherContext } from "../context/WeatherContext"
 import { fetchForecastData, fetchWeatherData } from "../service/weatherApi"
 import { ForecastPick } from "../models/Weather"
 
-let defaultLocation = "-23.520856837459185,-46.57482654740279"
-
 const useWeather = () => {
     const { location } = useContext(WeatherContext)
     const [weatherData, setWeatherData] = useState(null)
+    const [userLocation, setUserLocation] = useState("")
+    const [loading, setLoading] = useState<boolean>(false)
     const [forecastData, setForecastData] = useState<ForecastPick>()
     const [error, setError] = useState(null)
 
-    useEffect(() => {    
-            fetchWeatherData(location || defaultLocation)
-                .then(response => {
-                    if (response) {
-                        setWeatherData(response)
-                        fetchForecastData(response.location.name)
-                        .then(forecast => {
-                            if (forecast) setForecastData(forecast)
-                        })
-                        .catch(error => {
-                            setError(error)
-                        })
-                    }
-                })
-                .catch(error => {
-                    setError(error)
-                })
-    }, [location])
+    const handleGetUserLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(user => {
+                if (user) {
+                    setUserLocation(`${user.coords.latitude},${user.coords.longitude}`)
+                    setLoading(true)
+                } 
+            })
+        }
+    }
+
+    useEffect(() => { 
+                handleGetUserLocation()  
+                if (loading || location) {
+                    fetchWeatherData(location || userLocation)
+                    .then(response => {
+                        if (response) {
+                            setWeatherData(response)
+                            fetchForecastData(response.location.name)
+                            .then(forecast => {
+                                if (forecast) setForecastData(forecast)
+                            })
+                            .catch(error => {
+                                setError(error)
+                            })
+                        }
+                    })
+                    .catch(error => {
+                        setError(error)
+                    })
+                }
+    }, [location, loading])
     
     return {weatherData, forecastData, error}    
 }
