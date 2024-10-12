d27403d7c5fc7f7afb4e0ebd19908b255c1fa3e3
Tiago Borges <tiagoborges899@gmail.com>
2024-10-12 15:51:12 -0300

    feat: criação dos testes do SearchBard

--- src/_tests_/SearchBar.spec.tsx
+++ src/_tests_/SearchBar.spec.tsx
@@ -1,26 +1,30 @@
 import SearchBar from "../components/SearchBar/index"
-import {fireEvent, render} from "@testing-library/react"
+import { fireEvent, render } from "@testing-library/react"
 
-describe("SearchBar", () => {
-    it("should render SearchBar component", () => {
+describe("SearchBar Component", () => {
+    it("should render SearchBard", () => {
         render(<SearchBar />)
     })
 
-    it("should check if input locale is empty when SearchBar component is rended", () => {
-        const { getByPlaceholderText } = render(<SearchBar />)
+    it("should check form value is empty when component render", () => {
+        const { getByPlaceholderText} = render(<SearchBar />)
 
-        const cityName = getByPlaceholderText('Digite a localidade')
+        const inputCityName = getByPlaceholderText("Digite a localidade")
 
-        expect(cityName.getAttribute("value")).toBe("")
+        const value = inputCityName.getAttribute("value")
+
+        expect(value).toEqual("")
     })
 
-    it("should check if input city receive value", () => {
+    it("should check input form value is changing", () => {
         const { getByPlaceholderText } = render(<SearchBar />)
+        
+        const inputCityName = getByPlaceholderText("Digite a localidade")
 
-        const cityName = getByPlaceholderText('Digite a localidade')
+        fireEvent.change(inputCityName, {target: { value: "Rio de Janeiro" }})
 
-        fireEvent.change(cityName, {target: {value: "Paracambi"}})
+        const value = inputCityName.getAttribute("value")
 
-        expect(cityName.getAttribute("value")).toEqual("Paracambi")
+        expect(value).toEqual('Rio de Janeiro')
     })
 })
